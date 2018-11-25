94482e346bd4b5fea84677b89ef61c14d53c5ef9
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-11-25 19:07:09 +0300

    8 recyclerview adapterde data binding uygulandı

--- app/src/main/java/com/emrealtunbilek/databindingkotlin/fragments/MainFragment.kt
+++ app/src/main/java/com/emrealtunbilek/databindingkotlin/fragments/MainFragment.kt
@@ -6,7 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.emrealtunbilek.databindingkotlin.adapters.UrunRecyclerviewAdapter
 import com.emrealtunbilek.databindingkotlin.databinding.FragmentMainBinding
+import com.emrealtunbilek.databindingkotlin.models.Urun
+import com.emrealtunbilek.databindingkotlin.utils.TumUrunler
 
 
 class MainFragment : Fragment() {
@@ -18,7 +22,19 @@ class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
 
 
+        val mLayoutManager = GridLayoutManager(activity,2)
 
+        val urunler = TumUrunler()
+        val mAdapter = UrunRecyclerviewAdapter(urunler.tumUrunlerDizisi.toCollection(ArrayList<Urun>()), activity!!)
+
+        binding.urunListesi.layoutManager=mLayoutManager
+        binding.urunListesi.adapter=mAdapter
+
+        binding.refreshLayout.setOnRefreshListener {
+
+
+            binding.refreshLayout.isRefreshing=false
+        }
 
         return binding.root
     }
